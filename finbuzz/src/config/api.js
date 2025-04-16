dece7e6bb4213808b6755d9c3455518d4251b25f
udayan1611 <157144796+udayan1611@users.noreply.github.com>
2025-04-16 18:00:09 +0530

    Delete finbuzz directory

--- finbuzz/src/config/api.js
+++
@@ -1,7 +0,0 @@
-export const COINGECKO_API_KEY = "CG-RyJJ5BbzBEaVK13qBpN8sFX6";
-
-export const API_HEADERS = {
-  "Content-Type": "application/json",
-  "x-cg-demo-api-key": COINGECKO_API_KEY,
-};
-
